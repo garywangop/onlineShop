8805c039838c3ca9f2b15d888039a60aae36fd2f
garywangop <garywangop@gmail.com>
2020-02-05 01:27:41 -0500

    Change admin user email

--- src/main/java/onlineShop/SecurityConfig.java
+++ src/main/java/onlineShop/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth
-			.inMemoryAuthentication().withUser("stefanlaioffer@gmail.com").password("123").authorities("ROLE_ADMIN");
+			.inMemoryAuthentication().withUser("garywangop@gmail.com").password("123").authorities("ROLE_ADMIN");
 		
 		auth
 			.jdbcAuthentication()
